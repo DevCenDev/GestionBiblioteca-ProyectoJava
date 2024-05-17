dd1f4abf68cfdc76e59f0796efac0ee336ceff5e
DevCenDev <centurionllatasalexdaniel@gmail.com>
2024-05-17 16:00:54 -0500

    generador de ticket

--- ProyectoJava/src/Biblioteca/CompraPrestamoLibro.java
+++ ProyectoJava/src/Biblioteca/CompraPrestamoLibro.java
@@ -34,10 +34,8 @@ public class CompraPrestamoLibro{
         ImpTicket = sc.nextLine();
 
         if (ImpTicket.equalsIgnoreCase("S")) {
-            int aux = 1;
-            String nOp = String.valueOf(aux);
 
-            imprimirTicket = ticket.crearTicket(nOp, "MI BIBLIOTECA", nombreCliente, apellidosCliente,
+            imprimirTicket = ticket.crearTicket("001", "MI BIBLIOTECA", nombreCliente, apellidosCliente,
                     dni);
             System.out.println(imprimirTicket);
         } else if (ImpTicket.equalsIgnoreCase("N")) {
